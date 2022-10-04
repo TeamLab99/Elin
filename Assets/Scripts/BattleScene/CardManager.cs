25b7b05ff51cbe014de6f9602b6ecbb41e3d9413
시원 <96858320+NearthYou@users.noreply.github.com>
2022-10-04 16:21:54 +0900

    배틀 페이지
    10.4 작업 내용 중간 저장

---
+++ Assets/Scripts/BattleScene/CardManager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public static CardManager Inst { get; private set; }
+    void Awake() => Inst = this;
+
+    [SerializeField] ItemSO itemSO;
+    [SerializeField] GameObject cardPrefab;
+    [SerializeField] List<Card> myCards;
+    [SerializeField] List<Card> otherCards;
+    [SerializeField] Transform cardSpawnPoint;
+
+    List<Item> itemBuffer;
+
+    public Item PopItem()
+    {
+        if (itemBuffer.Count == 0)
+            SetupItemBuffer();
+
+        Item item = itemBuffer[0];
+        itemBuffer.RemoveAt(0);
+        return item;
+    }
+
+    private void SetupItemBuffer()
+    {
+        itemBuffer = new List<Item>();
+        for (int i = 0; i < itemSO.items.Length; i++)
+        {
+            Item item = itemSO.items[i];
+            for (int j = 0; j < item.percent; j++)
+                itemBuffer.Add(item);
+        }
+
+        for (int i = 0; i < itemBuffer.Count; i++)
+        {
+            int rand = Random.Range(i, itemBuffer.Count);
+            Item temp = itemBuffer[i];
+            itemBuffer[i] = itemBuffer[rand];
+            itemBuffer[rand] = temp;
+        }
+    }
+
+    private void Start()
+    {
+        SetupItemBuffer(); 
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+            AddCard(true);
+
+        if (Input.GetKeyDown(KeyCode.X))
+            AddCard(false);
+    }
+
+    void AddCard(bool isMine)
+    {
+        var cardObject = Instantiate(cardPrefab, cardSpawnPoint.position, Utils.QI);
+        var card = cardObject.GetComponent<Card>();
+        card.Setup(PopItem(), isMine);
+        (isMine ? myCards : otherCards).Add(card);
+
+        SetOriginOrder(isMine);
+        CardAlignment(isMine);
+    }
+
+    private void SetOriginOrder(bool isMine)
+    {
+        int count = isMine ? myCards.Count : otherCards.Count;
+        for (int i = 0; i < count; i++)
+        {
+            var targetCard = isMine ? myCards[i] : otherCards[i];
+            targetCard?.GetComponent<Order>().SetOriginOrder(i);
+        }
+    }
+
+    void CardAlignment(bool isMine)
+    {
+        var targetCards = isMine ? myCards : otherCards;
+        for (int i = 0; i < targetCards.Count; i++)
+        {
+            var targetCard = targetCards[i];
+
+            targetCard.originPRS = new PRS(Vector3.zero, Utils.QI, Vector3.one * 1.9f);
+            targetCard.MoveTransform(targetCard.originPRS, true, 0.7f);
+        }
+    }
+}
