3f8d18db63f7600cd121ebda681db6a3680e0faf
Sung Jun Cho <chosung0531@naver.com>
2022-12-20 15:38:06 +0900

    에셋추가 + 기본적인 이동구현
    점프 + 좌우 이동 + 기즈모(광선검출)까지 구현함

--- Assets/Scripts/Player/Player_Move.cs
+++ Assets/Scripts/Player/Player_Move.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class Player_Move : MonoBehaviour
 {
     Animator anim;
-    Rigidbody2D rigid;
-    
-    float moveDir;
-    bool isRight;
-    
+    Rigidbody2D rb;
+    public Transform groundChk;
+    public Transform wallChk;
+    public LayerMask groundLayer;
+    public LayerMask wallLayer;
 
+    public float groundDist;
+    public float wallDist;
+
+    float moveDir;
+    float jumpDir;
+    bool isRight;
+    bool isGround;
+    bool isWall;
     private void Awake()
     {
         anim = GetComponent<Animator>();
-        rigid = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Start()
@@ -25,16 +33,30 @@ public class Player_Move : MonoBehaviour
     
     void Update()
     {
-        
+        moveDir = Input.GetAxisRaw("Horizontal");
+        jumpDir = Input.GetAxis("Jump");
+        isGround = Physics2D.Raycast(groundChk.position, Vector2.down, groundDist, groundLayer);
     }
 
     void FixedUpdate()
     {
         Walk();
+        Jump();
     }
 
     private void Walk()
     {
-        moveDir = Input.GetAxisRaw("Horizotal");
+        rb.velocity = new Vector2(moveDir * 10, rb.velocity.y);
+    }
+    private void Jump()
+    {
+        if (isGround)
+            rb.velocity = new Vector2(rb.velocity.x, jumpDir * 10);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(groundChk.position,Vector2.down*groundDist);
+        Gizmos.DrawRay(wallChk.position, Vector2.right * wallDist*moveDir);
     }
 }
