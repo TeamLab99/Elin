577b641572f4f511ecca5224a69a5edfbad03a15
Sung Jun Cho <chosung0531@naver.com>
2022-12-21 20:55:45 +0900

    벽 슬라이드 점프 구현 + 애니메이션
    벽 슬라이드 점프는 구현 완료.
    애니메이션은 점프, 낙하, 좌우 이동, 평상시 4개 상태 구현
    점프/낙하 애니메이션이 약간 부자연스러움

--- Assets/Scripts/Player/Player_Move.cs
+++ Assets/Scripts/Player/Player_Move.cs
@@ -29,7 +29,7 @@ public class Player_Move : MonoBehaviour
         spr = GetComponent<SpriteRenderer>();
     }
 
-    void Start()
+    void Start() //초기 변수 제어
     {
         isRight = 1;
     }
@@ -41,10 +41,27 @@ public class Player_Move : MonoBehaviour
         jumpDir = Input.GetAxis("Jump");
         isGround = Physics2D.Raycast(groundChk.position, Vector2.down, groundDist, groundLayer);
         isWall = Physics2D.Raycast(wallChk.position, Vector2.right*isRight, wallDist, wallLayer);
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-            isRight = -1;
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-            isRight = 1;
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+                isRight = -1;
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+                isRight = 1;
+        } //좌우 표기
+
+        {
+            if (Mathf.Abs(moveDir) > 0.1f)
+                anim.SetBool("isRun", true);
+            else
+                anim.SetBool("isRun", false);
+            if (rb.velocity.y > 0.1f)
+                anim.SetBool("isFall", false);
+            else if (rb.velocity.y < -0.1f)
+                anim.SetBool("isFall", true);
+            if (isGround)
+                anim.SetBool("isGround", true);
+            else
+                anim.SetBool("isGround", false);
+        } //애니메이션 표기
     }
 
     void FixedUpdate()
@@ -53,51 +70,52 @@ public class Player_Move : MonoBehaviour
         {
             Walk();
             FlipX();
+            Jump();
+            WallSlide();
         }
-        Jump();
-        WallSlide();
     }
-    private void FreezX()
-    {
-        isWallJump=false;
-    }
+   
     private void WallSlide()
     {
         if (isWall)
         {
             isWallJump = false;
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.6f);
-            if (Input.GetAxis("Jump") != 0)
+            if (Input.GetAxis("Jump")!=0)
             {
                 isWallJump = true;
                 Invoke("FreezX", 0.3f);
                 isRight *= -1;
                 FlipX();
-                rb.velocity = new Vector2(rb.velocity.x * 5 * isRight, rb.velocity.y * 8);
+                rb.velocity = new Vector2(10*isRight, 8);
             }
         }
-    }
+    } //벽타기 + 벽점프
     private void Walk()
     {
         rb.velocity = new Vector2(moveDir * 10, rb.velocity.y);
-    }
+    } //걷기
     private void Jump()
     {
         if (isGround)
             rb.velocity = new Vector2(rb.velocity.x, jumpDir * 10);
-    }
+    } //점프
     private void FlipX()
     {
         if (isRight == -1)
             spr.flipX = true;
         else if(isRight== 1)
             spr.flipX = false;
-    }
+    } //좌우전환
+    private void FreezX()
+    {
+        isWallJump = false;
+    } //움직임 제어 풀기
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawRay(groundChk.position,Vector2.down*groundDist);
         Gizmos.color = Color.red;
         Gizmos.DrawRay(wallChk.position, Vector2.right * wallDist*isRight);
-    }
+    } //검출선
 }
