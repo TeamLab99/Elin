f3fb7221b83a7efefab411fe67654401b4039724
Sung Jun Cho <chosung0531@naver.com>
2022-12-24 16:45:54 +0900

    crank change
    crank change

--- Assets/Scripts/Player/Crank.cs
+++ Assets/Scripts/Player/Crank.cs
@@ -4,32 +4,54 @@ using UnityEngine;
 
 public class Crank : MonoBehaviour
 {
+    public GameObject crankMsg;
     SpriteRenderer spr;
     public GameObject crankWall;
     public Sprite crankUp;
     public Sprite crankDown;
     bool isCrankUp;
+    bool isCrankCollide;
+    bool isPullCrank;
     private void Awake()
     {
         spr=GetComponent<SpriteRenderer>();
         isCrankUp = true;
     }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.X))
+        {
+            if(isCrankCollide)
+                isPullCrank = true;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            if (isCrankUp)
+            isCrankCollide = true;
+            crankMsg.SetActive(true);
+            if (isCrankUp&&isPullCrank)
             {
                 crankWall.SetActive(false);
                 spr.sprite = crankDown;
                 isCrankUp = false;
             }
-            else
+            else if(!isCrankUp&&isPullCrank)
             {
                 crankWall.SetActive(true);
                 spr.sprite = crankUp;
                 isCrankUp = true;
             }
         }   
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isCrankCollide = false;
+            crankMsg.SetActive(false);
+            isPullCrank = false;
+        }
     }
 }
