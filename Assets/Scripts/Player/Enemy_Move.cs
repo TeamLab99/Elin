29f6878e58fdaef4d9166f45aca2ba919b749a90
Sung Jun Cho <chosung0531@naver.com>
2022-12-31 20:09:03 +0900

    Toward Player
    Toward Player

--- Assets/Scripts/Player/Enemy_Move.cs
+++ Assets/Scripts/Player/Enemy_Move.cs
@@ -6,23 +6,44 @@ using UnityEngine;
 public class Enemy_Move : MonoBehaviour
 {
     Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer spr;
     public int nextMove;
+    public GameObject exclamation;
+
+    private bool findPlayer;
+    public float speed;
+    public float distance;
+    public Transform player;
+    
     
     private void Awake()
     {
+        spr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
     private void Start()
     {
         Think();
     }
+    private void Update()
+    {
+    }
     private void FixedUpdate()
     {
-        Vector2 frontVec = new Vector2(rb.position.x + nextMove, rb.velocity.y);
-        rb.velocity = new Vector2(nextMove, rb.velocity.y);
-        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
-        if (rayHit.collider != null)
-            nextMove *= -1;
+        if (!findPlayer)
+            rb.velocity = new Vector2(nextMove, rb.velocity.y);
+        else
+        {
+            rb.velocity = new Vector2(0, 0);
+            TowardPlayer();
+            FlipDir();
+        }
+        // Vector2 frontVec = new Vector2(rb.position.x + nextMove, rb.velocity.y);
+        //RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        //  if (rayHit.collider != null)
+        //    nextMove *= -1;
     }
 
    void Think()
@@ -30,5 +51,41 @@ public class Enemy_Move : MonoBehaviour
         nextMove = Random.Range(-1, 2);
         Invoke("Think", 5f);
     }
+
+    void TowardPlayer()
+    {
+        if (Mathf.Abs(transform.position.x - player.position.x) > distance && findPlayer)
+        {
+            transform.Translate(new Vector2(-1, 0) * Time.deltaTime * speed);
+        }
+    }
     
+    void FlipDir()
+    {
+        if (transform.position.x - player.position.x < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            exclamation.SetActive(true);
+            findPlayer = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            exclamation.SetActive(false);
+            findPlayer = false;
+        }
+    }
 }
