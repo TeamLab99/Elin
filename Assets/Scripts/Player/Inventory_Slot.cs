e9279b9e934b5e4a3f0f25ffd6f1aafe581e32cb
Sung Jun Cho <chosung0531@naver.com>
2023-01-02 14:59:09 +0900

    인벤토리 패널만 생성
    인벤토리 패널만 생성

---
+++ Assets/Scripts/Player/Inventory_Slot.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory_Slot : MonoBehaviour
+{
+    public Image icon;
+    public Text itemName_Text;
+    public Text itemCount_Text;
+    public Text selected_Item;
+
+   /* public void AddItem(Item _item)
+    {
+        itemName_Text.text = _item.itemName;
+    }*/
+}
