d8cf17cf4a8d00fca4be993aae9d7806c1c17376
Sung Jun Cho <chosung0531@naver.com>
2023-01-19 16:58:07 +0900

    움직임 스크립트 새로 생성
    기존 스크립트 참조해서 수정중

--- Assets/Scripts/Player/Player/PlayerMove.cs
+++ Assets/Scripts/Player/Player/PlayerMove.cs
@@ -8,16 +8,20 @@ public class PlayerMove : MonoBehaviour
     Rigidbody2D rb;
     SpriteRenderer spr;
 
+    // 이동 관련 변수
+    public float xSpeed;
+    private float xInput;
+
     // 레이어 체크 변수
     private bool isGround;
     public float groundDist;
     public Transform groundCheck; 
     public LayerMask groundLayer;
-    
+
     // 점프 관련 변수
-    public float jumpPower;
-    public float jumpStartTime;
-    private float jumpTime;
+    public float ySpeed;
+    public float jumpTime;
+    private float jumpTimeCounter;
     private bool isJumping;
 
     private void Awake()
@@ -32,38 +36,54 @@ public class PlayerMove : MonoBehaviour
         
     }
 
-    private void Update()
-    {
-        isGround = Physics2D.Raycast(groundCheck.position, Vector2.down, groundDist, groundLayer);
-    }
-
     private void FixedUpdate()
     {
-        Jump();
+        xInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(xInput * xSpeed, rb.velocity.y);
     }
 
-    public void Jump()
+    private void Update()
     {
-        if ( Input.GetButtonDown("Jump"))
+        isGround = Physics2D.OverlapCircle(groundCheck.position, groundDist, groundLayer);
+
+        if (xInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (xInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        
+        if(isGround && Input.GetKeyDown(KeyCode.Space))
         {
             isJumping = true;
-            jumpTime = jumpStartTime;
-            rb.velocity = Vector2.up * jumpPower;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * ySpeed;
         }
-        if (Input.GetButton("Jump") && isJumping)
+        if (Input.GetKey(KeyCode.Space) && isJumping)
         {
-            if (jumpTime > 0)
+            if (jumpTimeCounter > 0)
             {
-                rb.velocity = Vector2.up * jumpPower;
-                jumpTime -= Time.deltaTime;
+                rb.velocity = Vector2.up * ySpeed;
+                jumpTimeCounter -= Time.deltaTime;
             }
             else
             {
                 isJumping = false;
             }
-            if (Input.GetButtonUp("Jump"))
-                isJumping = false;
         }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+            
+        //isGround = Physics2D.Raycast(groundCheck.position, Vector2.down, groundDist, groundLayer);
+    }
+
+
+    public void Jump()
+    {
     }
 
     private void OnDrawGizmos()
