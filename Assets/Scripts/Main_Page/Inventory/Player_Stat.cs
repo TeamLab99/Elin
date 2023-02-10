cc58b356e292bb64a2f663fb36eebb04bef86782
Sung Jun Cho <chosung0531@naver.com>
2023-02-10 14:40:13 +0900

    초기 스탯창 생성 완료
    소모품을 먹었을 때 hp,mp 증가
    플레이어가 데미지를 입었을 때 hp 감소
    플레이어가 장비를 착용, 해제했을 때 능력치 증감 구현했다.
    현재 능력치는 hp, mp, atk, def 4개로만 구성되어 있다.

--- Assets/Scripts/Main_Page/Inventory/Player_Stat.cs
+++ Assets/Scripts/Main_Page/Inventory/Player_Stat.cs
@@ -6,22 +6,19 @@ using UnityEngine.UI;
 public class Player_Stat : MonoBehaviour
 {
     public static Player_Stat instance;
-    public Text textHp;
+    public Text[] textState;
     // 현재 능력치
-    private int hp=50;
-    /*private int mp=50;
-    private int attack=5;
-    private int dffence=0;
+    private int hp=100;
+    private int mp=100;
+    public int atk=5;
+    public int def=0;
 
-    public int maxHp; 
-    public int maxMp;
-    */
+    public int currentHp=50; 
+    public int currentMp=50;
+    
     public void TakeDamage()
     {
         hp -= 10;
-        Debug.Log(hp);
-        textHp.text = hp.ToString();
-
     }
     void Start()
     {
@@ -29,6 +26,9 @@ public class Player_Stat : MonoBehaviour
     }
     void Update()
     {
-        
+        textState[0].text = currentHp.ToString();
+        textState[1].text = currentMp.ToString();
+        textState[2].text = atk.ToString();
+        textState[3].text = def.ToString();
     }
 }
--- Assets/Scripts/Main_Page/Manager/DataBase_Manager.cs
+++ Assets/Scripts/Main_Page/Manager/DataBase_Manager.cs
@@ -26,10 +26,10 @@ public class DataBase_Manager : MonoBehaviour
 
     void Start() 
     {
-        itemList.Add(new Item_Data(10001, "cherry", "체력을 10 증가", Item_Data.ItemType.Consumer,0));
-        itemList.Add(new Item_Data(10002, "gem", "체력을 30 증가", Item_Data.ItemType.Consumer,0));
-        itemList.Add(new Item_Data(20001, "bag", "방어력 10 증가", Item_Data.ItemType.Equipment,0));
-        itemList.Add(new Item_Data(20002, "belt", "방어력 30 증가", Item_Data.ItemType.Equipment,0));
+        itemList.Add(new Item_Data(10001, "cherry", "체력을 10 증가", Item_Data.ItemType.Consumer,0,0,10));
+        itemList.Add(new Item_Data(10002, "gem", "마나를 10 증가", Item_Data.ItemType.Consumer,0,0,0,10));
+        itemList.Add(new Item_Data(20001, "bag", "방어력 10 증가", Item_Data.ItemType.Equipment,0,10));
+        itemList.Add(new Item_Data(20002, "belt", "공격력 10 증가", Item_Data.ItemType.Equipment,10));
         itemList.Add(new Item_Data(30001, "bone", "해골몬스터의 뼈조각", Item_Data.ItemType.Quest,0));
         itemList.Add(new Item_Data(30002, "egg", "닭 몬스터의 알", Item_Data.ItemType.Quest,0));
     } // 아이템 정보를 입력
