12149ca94c47ddaab1990314b25e812dc93be56c
시원 <96858320+NearthYou@users.noreply.github.com>
2023-02-15 18:39:09 +0900

    몬스터, 스킬 스크립트 수정

--- Assets/Scripts/BattleScene/Battle.cs
+++ Assets/Scripts/BattleScene/Battle.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,92 +17,81 @@ public class Battle : MonoBehaviour
 
     // 인스펙터 창에서 Entity 받아오기
     [Header("Entity")]
-    [SerializeField] Entity player;
+    [SerializeField] Player player;
     [SerializeField] Monster monster;
 
-    // 행동 게이지 수치 관련
+    // 행동 게이지 UI
     [Header("UI")]
     public Image scroll;
-    float maxTime;
-    static float curTime;
-    int count;
-    WaitForSeconds delay15 = new WaitForSeconds(1.5f);
 
-    // 배틀 종료 판단
-    public bool isDie;
-    bool gaugeStop;
+    float maxTime;
+    int count;
+    static float curTime;
+    public bool stopGauge;
+
+    WaitForSeconds delay15 = new WaitForSeconds(1.5f);
 
     private void Start()
     {
+        MonsterSkill.Inst.SetEntites(player, monster);
         maxTime = monster.GetAttackSpeed();
-        // 게이지 최대시간 입력
-        curTime = maxTime;
         count = monster.GetSkillCount();
+        curTime = maxTime;
     }
+
     private void Update()
     {
         // 게이지의 상태 계속 업데이트
         scroll.fillAmount = curTime / maxTime;
 
         // 누군가 죽지 않았다면 계속 검사
-        if (!isDie)
+        if (!stopGauge)
         {
-            if(!gaugeStop)
-                curTime -= Time.deltaTime;
-            
+            curTime -= Time.deltaTime;
             if (curTime <= 0)
             {
                 if (count > 0)
                 {
                     // 게이지 재충전, 어택 애니메이션, 공격 함수
                     curTime = maxTime;
-                    monster.MonsterHitEffectWithAttack(player);
+                    monster.StartCoroutine("MonsterHitEffectWithAttack",player);
                     count--;
                 }
                 else if (count == 0)
                 {
                     if (maxTime > 1f)
                     {
-                        gaugeStop = true;
-                        isDie = true;
-                        maxTime -= 0.5f;
-                        EffectManager.Inst.MonsterSkillEffectOn();
+                        stopGauge = true;
+                        monster.UseSkill(1);
                         StartCoroutine(MonsterSkillDelay());
+                        maxTime = monster.GetAttackSpeed();
                     }
                     count = monster.GetSkillCount();
                 }
             }
         }
-
     }
 
     public IEnumerator MonsterSkillDelay()
     {
         yield return delay15;
-
         curTime = maxTime;
-
-        gaugeStop = false;
-        isDie = false;
+        stopGauge = false;
     }
 
     public void GameOver()
     {
-        // 턴 및 카드선택 정지
+        // 턴 및 카드 선택 정지
+        stopGauge = true;
         TurnManager.Inst.isLoading = true;
         CardManager.Inst.SetIsCardMoving(true);
         CardManager.Inst.AllEnlargeCancle();
-        isDie = true;
-
-        // 게임 오버 알림
         Debug.Log("게임 오버!");
     }
 
     public void PlayerAttack()
     {
         player.Attack(monster);
-
-        
     }
 
     public void PlayerHeal()
