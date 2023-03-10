8b703dc9c62b057c4de52b7a40d31fc79d1a3e89
Sung Jun Cho <chosung0531@naver.com>
2023-03-10 23:43:09 +0900

    대화창 간단 구현
    대화 판넬 생성 + 스프라이트 이미지 다운

---
+++ Assets/Scripts/Main_Page/Text/Text_Manager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text_Manager : MonoBehaviour
+{
+  
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
