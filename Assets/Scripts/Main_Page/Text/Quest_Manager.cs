869c459c01145068a780aca33badc88b69f817c9
Sung Jun Cho <chosung0531@naver.com>
2023-03-11 17:52:24 +0900

    대화창 상호작용 + json파일 생성
    대화창 상호작용 + json파일 생성

--- Assets/Scripts/Main_Page/Text/Quest_Manager.cs
+++ Assets/Scripts/Main_Page/Text/Quest_Manager.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Text_Manager : MonoBehaviour
+public class Quest_Manager : MonoBehaviour
 {
-  
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-  
+    // Update is called once per frame
     void Update()
     {
         
