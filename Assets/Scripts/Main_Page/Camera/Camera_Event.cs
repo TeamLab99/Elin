a171ed08b993bcfad6cb829a80aaddfec15dac1a
Sung Jun Cho <chosung0531@naver.com>
2023-03-18 22:22:52 +0900

    카메라 이번트 조건 추가
    카메라 이번트 조건 추가

--- Assets/Scripts/Main_Page/Camera/Camera_Event.cs
+++ Assets/Scripts/Main_Page/Camera/Camera_Event.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Camera_Event : MonoBehaviour
 {
     public Vector3 DesVec;
+    private bool isTry=true;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && isTry)
         {
             DataBase_Manager.instance.cm.InputVec(DesVec);
             DataBase_Manager.instance.cm.followType = Camera_Follow.FollowType.GotoObject;
+            isTry = false;
         }
     }
           
