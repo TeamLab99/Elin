c30a479858e751faea0bad33a6649acc1f2a6e10
시원 <96858320+NearthYou@users.noreply.github.com>
2023-05-18 18:09:53 +0900

    덱 크리에티터 씬 삭제 및 UI씬으로 이사

--- Assets/02. Scripts/Deck/Scenes/BaseScene.cs
+++ Assets/02. Scripts/Deck/Scenes/BaseScene.cs
@@ -20,7 +20,7 @@ public abstract class BaseScene : MonoBehaviour
         if (obj == null)
             //새로운 씬에 들어왔다고 했을 때 SceneMangerEx에 의해서 Init되는데 그 때 이벤트 시스템이 존재하지 않을 경우
             //이벤트 시스템을 씬에 인스턴시에트해줌
-            Managers.Resource.Instantiate("DeckCreator/EventSystem").name = "@EventSystem";
+            Managers.Resource.Instantiate("EventSystem").name = "@EventSystem";
     }
 
     public abstract void Clear();
