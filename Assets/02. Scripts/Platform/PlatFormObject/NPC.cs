912d73ea0edb3e41c1fe786856ee1fba84eecb81
Sung Jun Cho <chosung0531@naver.com>
2023-05-23 14:56:52 +0900

    얀 에러 해결중..(테스트중)
    얀 에러 해결중..(테스트중)

--- Assets/02. Scripts/Platform/PlatFormObject/NPC.cs
+++ Assets/02. Scripts/Platform/PlatFormObject/NPC.cs
@@ -6,6 +6,8 @@ using Yarn.Unity;
 public class NPC : InteractObject
 {
     [SerializeField] string npcName;
+    public DialogueRunner dd;
+    
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -23,4 +25,29 @@ public class NPC : InteractObject
         }
     }
 
+    [YarnCommand("ttt")]
+    public static void CoutLog(string num)
+    {
+        Debug.Log(num);
+        Debug.Log(int.Parse(num));
+        Debug.Log("커맨드 함수 작동중");
+    }
+
+    [YarnCommand("kkk")]
+    public static void Use()
+    {
+        int cnt = 2;
+        Dictionary<int,int> itemList = new Dictionary<int, int>();
+        itemList.Add(101, 5);
+        itemList.Add(102, 3);
+        if (itemList.ContainsKey(101))
+        {
+            itemList[101] -= cnt;
+            Debug.Log(itemList[101]);
+        }
+        else
+        {
+            Debug.Log("못찾음");
+        }
+    }
 }
