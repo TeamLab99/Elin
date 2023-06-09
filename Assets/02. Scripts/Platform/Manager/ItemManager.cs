63009ca705ea1e1020e158b38e9d9a801971448f
Sung Jun Cho <chosung0531@naver.com>
2023-06-09 21:36:10 +0900

    장비 착용, 해제 문제 해결
    장비 착용, 해제 문제 해결

--- Assets/02. Scripts/Platform/Manager/ItemManager.cs
+++ Assets/02. Scripts/Platform/Manager/ItemManager.cs
@@ -6,7 +6,8 @@ public class ItemManager : Singleton<ItemManager>
 {
     public Dictionary<int, Items> allItemDataBase; // 전체 아이템 데이터들
     public Dictionary<int, Items> holdItemDataBase; // 들고 있는 아이템들
-    public Dictionary<int, Items> wearItemDataBase; // 입고 있는 아이템들
+    public List<Items> holdEquipList;
+    public List<Items> wearEquipList;
     
     private InvenUI invenUI;
     private bool canUseItem = true;
@@ -14,63 +15,67 @@ public class ItemManager : Singleton<ItemManager>
 
     void Start()
     {
-        SetUpItemDictionary();
+        SetUpItemDataBase();
     }
 
-    private void SetUpItemDictionary()
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            GetItem(101);
+            GetItem(101);
+            GetItem(102,5);
+            GetItem(201);
+            GetItem(301,3);
+            GetItem(202);
+            GetItem(202);
+            GetItem(204);
+        }
+    }
+
+    private void SetUpItemDataBase()
     {
         allItemDataBase = Managers.Data.ItemDict;
-        holdItemDataBase = Managers.Data.ItemDict;
-        //holdItemDataBase = new Dictionary<int, Items>();
+        holdItemDataBase = new Dictionary<int, Items>();
+        holdEquipList = new List<Items>();
+        wearEquipList = new List<Items>();
     }
 
     public void GetItem(int _itemID, int _itemCnt = 1)
     {
-        if (holdItemDataBase.ContainsKey(_itemID))
-            holdItemDataBase[_itemID].itemCnt += _itemCnt;
-        else
+        if (_itemID / 100 != 2)  // 소모품, 기타 아이템
         {
-            holdItemDataBase.Add(_itemID, holdItemDataBase[_itemID]);
-            holdItemDataBase[_itemID].itemCnt += _itemCnt - 1;
+            if (holdItemDataBase.ContainsKey(_itemID))
+                holdItemDataBase[_itemID].itemCnt += _itemCnt;
+            else
+            {
+                holdItemDataBase.Add(_itemID, allItemDataBase[_itemID]);
+                holdItemDataBase[_itemID].itemCnt += _itemCnt - 1;
+            }   
+        }
+        else // 장비 아이템
+            holdEquipList.Add(allItemDataBase[_itemID]);
+    }
+
+    public void UseItem(int _itemID, int _itemCnt=1) // 기타, 소모품만 사용
+    {
+        if (_itemID / 100 != 2)
+        {
+            if (holdItemDataBase.ContainsKey(_itemID))
+            {
+                holdItemDataBase[_itemID].itemCnt -= _itemCnt;
+                UseItemEffect(_itemID);
+                DeleteItem(_itemID);
+            }
         }
     }
 
-    public void StartUseItem(int _itemID, int _itemCnt=1)
-    {
-        StartCoroutine(UseItem(_itemID, _itemCnt));
-    }
-
-    IEnumerator UseItem(int _itemID, int _itemCnt = 1)
-    {
-        if (holdItemDataBase.ContainsKey(_itemID) && canUseItem)
-        {
-            holdItemDataBase[_itemID].itemCnt -= _itemCnt;
-            ItemEffect(_itemID);
-            DeleteItem(_itemID);
-            canUseItem = false;
-            yield return itemCoolTime;
-            canUseItem = true;
-        }
-    }
-
-    public void ItemEffect(int _itemID, int _sign=1)
+    public void UseItemEffect(int _itemID, int _sign=1)
     {
         switch (holdItemDataBase[_itemID].itemEffect)
         {
             case "plusHP":
                 PlayerStat.instance.HealPlayer(holdItemDataBase[_itemID].itemFigure* _sign);
-                break;
-            case "maxHP":
-                PlayerStat.instance.ChangeStat(holdItemDataBase[_itemID].itemFigure * _sign,0,0,0);
-                break;
-            case "attackPower":
-                PlayerStat.instance.ChangeStat(0,holdItemDataBase[_itemID].itemFigure * _sign,0,0);
-                break;
-            case "maxCost":
-                PlayerStat.instance.ChangeStat(0,0,holdItemDataBase[_itemID].itemFigure * _sign,0);
-                break;
-            case "costRecoverySpeed":
-                PlayerStat.instance.ChangeStat(0,0,0,holdItemDataBase[_itemID].itemFigure * _sign);
                 break;
             default:
                 break;
@@ -80,23 +85,51 @@ public class ItemManager : Singleton<ItemManager>
     public void DeleteItem(int _itemID)
     {
         if (holdItemDataBase[_itemID].itemCnt <= 0)
-        {
             holdItemDataBase.Remove(_itemID);
+        invenUI.ClassificationItems();
+    }
+
+    public void TakeOnEquipment(Items _equipItem, int _itemIdx)
+    {
+        if (invenUI.wearSlotSize < 4)
+        {
+            holdEquipList.RemoveAt(_itemIdx);
+            wearEquipList.Add(_equipItem);
+            EquipItemEffect(_equipItem);
             invenUI.ClassificationItems();
+            invenUI.WearEquipmentItems();
         }
-            
     }
 
-    public void TakeOnEquipment()
+    public void TakeOffEquipment(Items _equipItem)
     {
-
+        holdEquipList.Add(_equipItem);
+        wearEquipList.Remove(_equipItem);
+        EquipItemEffect(_equipItem, -1);
+        invenUI.ClassificationItems();
+        invenUI.WearEquipmentItems();
     }
 
-    public void TakeOffEquipment(int _itemID)
+    public void EquipItemEffect(Items _equipEffect, int _sign = 1)
     {
-        ItemEffect(_itemID, -1);
+        switch (_equipEffect.itemEffect)
+        {
+            case "maxHP":
+                PlayerStat.instance.ChangeStat(_equipEffect.itemFigure * _sign, 0, 0, 0);
+                break;
+            case "attackPower":
+                PlayerStat.instance.ChangeStat(0, _equipEffect.itemFigure * _sign, 0, 0);
+                break;
+            case "maxCost":
+                PlayerStat.instance.ChangeStat(0, 0, _equipEffect.itemFigure * _sign, 0);
+                break;
+            case "costRecoverySpeed":
+                PlayerStat.instance.ChangeStat(0, 0, 0, _equipEffect.itemFigure * _sign);
+                break;
+            default:
+                break;
+        }
     }
-
     public void RegisterInvenUI(InvenUI _invenUI)
     {
         invenUI = _invenUI;
--- Assets/02. Scripts/Platform/UI/Inven/InvenUI.cs
+++ Assets/02. Scripts/Platform/UI/Inven/InvenUI.cs
@@ -13,19 +13,21 @@ public class InvenUI : MonoBehaviour
 {
     public Slot[] itemSlots;
     public Transform itemSlotsTransform;
+    public EquipSlot[] equipSlots;
+    public Transform equipSlotsTransform;
     public ItemTypes itemTypeEnum = ItemTypes.Con;
-   
+
     private int slotSize=0;
+    public int wearSlotSize = 0;
     private string itemTypeString;
     private InvenTabSlot[] invenTabSlots;
-    private Dictionary<int, Items> invenItems; // 인벤토리에 저장된 아이템
     private List<Items> showItemList = new List<Items>(); // 보여지는 아이템들
-    private Dictionary<int, Items> wearEquipDic = new Dictionary<int, Items>(); // 착용한 장비 아이템
-
+    private List<Items> showWearList = new List<Items>(); // 착용한 아이템들
 
     private void Awake()
     {
         itemSlots = itemSlotsTransform.GetComponentsInChildren<Slot>();
+        equipSlots = equipSlotsTransform.GetComponentsInChildren<EquipSlot>();
         invenTabSlots = GetComponentsInChildren<InvenTabSlot>();
         ItemManager.instance.RegisterInvenUI(this);
     }
@@ -34,10 +36,21 @@ public class InvenUI : MonoBehaviour
     {
         showItemList.Clear();
         itemTypeString = itemTypeEnum.ToString();
-        foreach (KeyValuePair<int,Items> itemList in ItemManager.instance.holdItemDataBase)
+        switch (itemTypeEnum)
         {
-            if (itemList.Value.itemType == itemTypeString)
-                showItemList.Add(itemList.Value);
+            case ItemTypes.Equ:
+                for(int i=0; i< ItemManager.instance.holdEquipList.Count; i++)
+                {
+                    showItemList.Add(ItemManager.instance.holdEquipList[i]);
+                }
+                break;
+            default:
+                foreach (KeyValuePair<int, Items> itemList in ItemManager.instance.holdItemDataBase)
+                {
+                    if (itemList.Value.itemType == itemTypeString)
+                        showItemList.Add(itemList.Value);
+                }
+                break;
         }
         ShowItem();
     }
@@ -48,7 +61,7 @@ public class InvenUI : MonoBehaviour
         for (int i = 0; i < slotSize; i++)
         {
             itemSlots[i].gameObject.SetActive(true);
-            itemSlots[i].AddItem(showItemList[i]);
+            itemSlots[i].AddItem(showItemList[i],i);
         }
         for(int i=slotSize; i < itemSlots.Length; i++)
         {
@@ -58,18 +71,40 @@ public class InvenUI : MonoBehaviour
     }
 
 
-    public void ChangeTab(ItemTypes _itemType)
+    public void WearEquipmentItems()
     {
-        itemTypeEnum = _itemType;
-        ClassificationItems();
-        for(int i=0; i<invenTabSlots.Length; i++)
-            invenTabSlots[i].SelectButtonEffect();
+        showWearList = ItemManager.instance.wearEquipList;
+        ShowEquipment();
+    }
+
+    public void ShowEquipment()
+    {
+        wearSlotSize = showWearList.Count;
+        for (int i = 0; i < wearSlotSize; i++)
+        {
+            equipSlots[i].gameObject.SetActive(true);
+            equipSlots[i].AddItem(showWearList[i]);
+        }
+        for (int i = wearSlotSize; i <4; i++)
+        {
+            equipSlots[i].RemoveItem();
+            equipSlots[i].gameObject.SetActive(false);
+        }
     }
 
     private void OnEnable()
     {
         ClassificationItems();
+        WearEquipmentItems();
         //StartCoroutine("LoadingItemList");
+    }
+
+    public void ChangeTab(ItemTypes _itemType)
+    {
+        itemTypeEnum = _itemType;
+        ClassificationItems();
+        for (int i = 0; i < invenTabSlots.Length; i++)
+            invenTabSlots[i].SelectButtonEffect();
     }
 
     /*IEnumerator LoadingItemList()
