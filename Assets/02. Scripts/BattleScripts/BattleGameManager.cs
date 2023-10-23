bdba9d53f8e9a366ac607b75734965f14525d59a
sung jun <chosung0531@naver.com>
2023-10-23 21:05:12 +0900

    [Mod] 나는야 조성준

--- Assets/02. Scripts/BattleScripts/BattleGameManager.cs
+++ Assets/02. Scripts/BattleScripts/BattleGameManager.cs
@@ -62,7 +62,6 @@ public class BattleGameManager : Singleton<BattleGameManager>
 
         mainCamera.GetComponent<CamerEffect>().enabled = false;
         player.GetComponent<PlayerController>().ControlPlayer(false);
-        player.GetComponent<PlayerController>().anim.SetBool("Walk", false);
 
         var targetPos = new Vector3(Mathf.Lerp(player.transform.position.x, mobPos.x, 0.5f), mobPos.y + 5f, -150);
         mainCamera.transform.DOMove(targetPos, 1.5f).SetEase(ease);
